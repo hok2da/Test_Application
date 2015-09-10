47f74047e6f5e7bbb2ff3cb7a55f2a20d02cf580
Hassan Khan <hok2da@virginia.edu>
2015-09-09 21:32:38 -0400

    commit from terminal

--- app/src/main/java/com/notone/test_application/MainActivity.java
+++ app/src/main/java/com/notone/test_application/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int i = 9;
+        int j = 123;
     }
 
     @Override
